670ee216fd1fe83d67ab595ec1e5281defb6cd35
miryarik <85996187+miryarik@users.noreply.github.com>
2025-02-23 00:33:37 +0530

    ready to go setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import imgSrc from "./images/kode.jpg";
+
+const image = document.createElement("img");
+
+image.src = imgSrc;
+
+document.body.appendChild(image);
+
+console.log("okayyyyy");
